802b0c2f260a5a054b41b6458cfe74ae810e8e9c
David <sambunjak.david@gmail.com>
2025-08-26 01:55:09 +0200

    Solve exercise 08_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  let arr = str.split("");
+  let result = [];
+  for (let i = str.length - 1; i >= 0; i--) {
+    result.push(arr[i]);
+  }
+  console.log(result);
+  result = result.join("");
+  console.log(result);
+  return result;
 };
 
 // Do not edit below this line
