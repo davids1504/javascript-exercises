07bcd5e69c93de3dbf15525e13d632ddb1366761
David <sambunjak.david@gmail.com>
2025-08-26 01:36:58 +0200

    Solved exercise 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (str, multiplier) {
+  if (multiplier < 0) return "ERROR";
+  let result = "";
+  for (let i = 0; i < multiplier; i++) {
+    result += str;
+  }
+  return result;
 };
 
 // Do not edit below this line
