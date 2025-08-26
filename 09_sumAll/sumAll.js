c761a0c2510347034fa93f1d4f8f5b7813947d0a
David <sambunjak.david@gmail.com>
2025-08-26 20:54:03 +0200

    Solve exercises 09 and 10

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if (a < 0 || b < 0) return "ERROR";
+  if (!Number.isInteger(a) || !Number.isInteger(b)) {
+    return "ERROR";
+  }
+  let higher = b;
+  let lower = a;
 
+  if (a > b) {
+    higher = a;
+    lower = b;
+  }
+
+  let sum = lower;
+  for (let i = lower + 1; i <= higher; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
