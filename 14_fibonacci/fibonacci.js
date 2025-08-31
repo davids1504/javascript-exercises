8b25648a2b5876ca62eddea6516d3e95bad7e1d0
David <sambunjak.david@gmail.com>
2025-08-31 02:40:26 +0200

    Solve exercise 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -2,10 +2,11 @@ const fibonacci = function (num) {
   if (+num === 0) return 0;
   if (+num === 1) return 1;
   if (+num === 2) return 1;
+  if (+num < 0) return "OOPS";
   let a = 1;
   let b = 1;
   let c;
-  for (let i = 0; i < num; i++) {
+  for (let i = 2; i < num; i++) {
     c = b;
     b += a;
     a = c;
